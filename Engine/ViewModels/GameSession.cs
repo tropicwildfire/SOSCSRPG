8d94aaa50edb05fab0a60aaaefd73145667dc715
tropicwildfire <tropicwildfire@gmail.com>
2025-01-21 13:28:09 -0600

    1/21/25

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Scott";
+            CurrentPlayer.Gold = 100000;
+        }
+    }
+}
